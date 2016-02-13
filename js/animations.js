317bfa2ce3e39a936df9fbb1436ceb2c523b8ce9
ericslc <eric.leatham@gmail.com>
2016-02-12 17:08:49 -0700

    working on mimi-menu

--- js/animations.js
+++ js/animations.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
 
   $('.tweet-compose').blur(function(){
     $('.tweet-compose').height('21px');
-      $('#tweet-controls').hide('click');
   });
     var maxLength = 140;
     $('#tweet-place').keyup(function () {
@@ -16,11 +15,26 @@ $(document).ready(function(){
      $('#chars').text(length);
      if (length < 11) {
        $('#chars').addClass('red');
-     }
+     };
      if (length > 10) {
        $('#chars').removeClass('red');
      };
+     if(length >= 0){
+       $('#tweet-submit').prop('disabled', false);
+     }
+     else if(length <= -1){
+       $('#tweet-submit').prop('disabled', true);
+     };
+$('#tweet-submit').on('click', function(){
+$('#stream').prepend($('#tweet-controls'))
+$('#stream').prepend($('#tweet-place'));
+$('#stream').prepend($('#dashboard'));
+$('ul.mini li span').hide();
+$('')
+
+});
    });
+
 
 
 
